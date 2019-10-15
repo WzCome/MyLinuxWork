2b7386a629b2411aeb14c3cdf9ff08cdf86f2395
wz <2604802298@qq.com>
2019-10-15 16:24:11 +0800

    w :wq :q
    qq
    :q

--- test5/main.c
+++ test5/main.c
@@ -1,30 +1,5 @@
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#define MAX 100
+#include "head.h"
 int m[MAX];
-void urand(int *a){
-	int i;
-	srand((unsigned)time(NULL));
-	for(i=0;i<MAX;i++){
-		a[i]=(int)(rand()%1000+1);
-	}
-}
-
-int max(int *a){
-int i;
-int smax=-1;
-for(i=0;i<MAX;i++){
-		if(a[i]>smax)
-{
-smax=a[i];
-}
-	}
-return smax;
-}
-
 int main(){
 urand(m);
 printf("%d\n",max(m));
