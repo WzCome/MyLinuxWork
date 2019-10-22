b491295353047546d754752b2902ba172f36cf4c
wz <2604802298@qq.com>
2019-10-22 14:27:32 +0800

    newcode

--- code/head.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#define MAX 100
-void urand(int *a);
-int max(int *a);
--- code/main.c
+++
@@ -1,7 +0,0 @@
-#include "head.h"
-int m[MAX];
-int main(){
-urand(m);
-printf("max=%d\n",max(m));
-return 0;
-}
