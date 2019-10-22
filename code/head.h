b5fcc5605da9d30f92106491364f077421975c15
wz <2604802298@qq.com>
2019-10-22 16:28:11 +0800

    newcode

---
+++ code/head.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dlfcn.h>
+#define MAX 100
+void urand(int *a);
+int max(int *a);
+int sum(int *a);
--- code/main.c
+++ code/main.c
@@ -3,5 +3,6 @@ int m[MAX];
 int main(){
 urand(m);
 printf("max=%d\n",max(m));
+printf("sum=%d\n",sum(m));
 return 0;
 }
