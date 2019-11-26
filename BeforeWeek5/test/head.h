5f4e73e31963129442b15c0f6059fb498458fc4b
wz <2604802298@qq.com>
2019-11-26 20:20:11 +0800

    s

--- BeforeWeek5/test/head.h
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<time.h>
-#include<stdlib.h>
-#include<unistd.h>
-#define MAX 10000
-void usort(int *,int);
-int compar(const void *,const void *);
-void urand(int *,int *, int);
